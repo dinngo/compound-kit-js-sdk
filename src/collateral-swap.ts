d0b933ca5816b17d24553ad4b1a844d3777874f8
Jeff Huang <jeff@dinngo.co>
2023-08-03 11:16:17 +0800

    feat: add buildCollateralSwapTransactionRequest and params

---
+++ src/collateral-swap.ts
@@ -0,0 +1,32 @@
+import { Position, QuoteAPIResponseBody } from './types';
+import * as apisdk from '@protocolink/api';
+import { buildRouterTransactionRequest, quote } from './api';
+import * as common from '@protocolink/common';
+
+export interface CollateralSwapParams {
+  account: string;
+  withdrawalToken: common.TokenObject;
+  amount: string;
+  targetToken: common.TokenObject;
+  slippage?: number;
+}
+
+export interface CollateralSwapQuotation {
+  targetTokenAmount: string;
+  currentPosition: Position;
+  targetPosition: Position;
+}
+
+export async function getCollateralSwapQuotation(
+  chainId: number,
+  marketId: string,
+  params: CollateralSwapParams
+): Promise<QuoteAPIResponseBody<CollateralSwapQuotation>> {
+  return quote(chainId, marketId, 'collateral-swap', params);
+}
+
+export async function buildCollateralSwapTransactionRequest(
+  routerData: Pick<apisdk.RouterData, 'chainId' | 'account' | 'logics' | 'referralCode'>
+) {
+  return buildRouterTransactionRequest(routerData);
+}
